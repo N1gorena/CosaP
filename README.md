46a52df511041fcddc13a28af59dff2f6a9f454d
Nicolas Guangorena <guangore@usc.edu>
2018-12-05 20:05:37 -0800

    Added sql db for Ubuntu Server
    Updated readme and appdb upload.

--- README.md
+++ README.md
@@ -1,10 +1,54 @@
 # CosaP
 
 This program can be run in an Eclipse environment. It is assumed that the program will be run on Eclipse 2018-2019 version.
-It is expected that Google Web Toolkit will be installed via the Eclipse marketplace.
+It is expected that Google Web Toolkit will be installed via the Eclipse marketplace. It is expected that a remote/local db is available to connect to.
+Remote/local db uses repo db.
 
 Otherwise all neccessary files are available with this repo download.
 
 Overview
 
-#TODO
+##File Structure
+####>.metadata
+-->Purpose: Eclipse Workspace. Nothing to concern self with.
+####>Hello Triangle
+-->Purpose: Eclipse project. Source of the program. Everything built around this.
+######---->.settings
+------>Eclipse fluff.
+######---->Shaders
+------>Directory for shader program src files.
+######---->bin
+------>binary fluff.
+######---->src
+**------>guiPackage**
+-------->StrokeListener
+---------->Class that handles user I/O.
+**------>mainPackage**
+-------->FaceNormal
+---------->Class holding normal vector
+-------->ModelObject
+---------->Class representing a 3d model
+-------->ObjectFace
+---------->Class representing 3 vertices that make a face.
+-------->StaticClass
+---------->Main class, main method.
+-------->Vertice
+---------->Class holding xyz coords.
+######---->tmp
+------>fluff
+---->.classpath
+------>fluff
+---->.project
+------>fluff
+####>JOML - Java open math library
+-->Purpose: This directory contains the joml library in a maven project. Import the project for use as Joml library. Will need to delete sections of pom.xml. They will show up as errors after importing maven project.
+Put this imported project in classpath.
+####>SWT
+-->Purpose: This directory has the SWT jar to add to classpath.
+####html
+-->Purpose: This directory holds the /var/www/html folder located on an Ubuntu 18.04 server VM that the program uses as a src to download 3d model files.
+####lwjgl-release-3.2.0-custom
+-->Purpose: This directory holds all neccessary jars for lightweight java opengl.
+
+##Things To Do on startup
+1. Check that vm holding sql tables ifconfig address is correct in src code.
---
+++ appDb.sql
@@ -0,0 +1,79 @@
+-- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
+--
+-- Host: localhost    Database: appMain
+-- ------------------------------------------------------
+-- Server version	5.7.24-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `files`
+--
+
+DROP TABLE IF EXISTS `files`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `files` (
+  `fId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `mId` bigint(20) NOT NULL,
+  `fileLoc` varchar(256) DEFAULT NULL,
+  PRIMARY KEY (`fId`),
+  UNIQUE KEY `fileLoc` (`fileLoc`),
+  KEY `mId` (`mId`),
+  CONSTRAINT `files_ibfk_1` FOREIGN KEY (`mId`) REFERENCES `models` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `files`
+--
+
+LOCK TABLES `files` WRITE;
+/*!40000 ALTER TABLE `files` DISABLE KEYS */;
+INSERT INTO `files` VALUES (1,2,'modelFiles/cube.obj'),(2,3,'modelFiles/pyr.obj'),(3,1,'modelFiles/sphere.obj'),(4,4,'modelFiles/Horse.obj'),(5,4,'modelFiles/test.obj');
+/*!40000 ALTER TABLE `files` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `models`
+--
+
+DROP TABLE IF EXISTS `models`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `models` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(256) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `models`
+--
+
+LOCK TABLES `models` WRITE;
+/*!40000 ALTER TABLE `models` DISABLE KEYS */;
+INSERT INTO `models` VALUES (1,'Sphere'),(2,'Cube'),(3,'Pyramid'),(4,'horse');
+/*!40000 ALTER TABLE `models` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-12-06  3:59:50
